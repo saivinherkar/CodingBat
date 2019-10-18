59b19b3f2f7abe13c7a279c27585fbc699e15851
Sai Vinherkar <36626703+saivinherkar@users.noreply.github.com>
2019-10-18 01:58:54 -0400

    Add files via upload

---
+++ CodingBat_Map.java
@@ -0,0 +1,27 @@
+//--Map-2 > word---
+//Given an array of strings, return a Map<String, Integer> containing a key for every different string in the array, always with the value 0. For example the string "hello" makes the pair "hello":0. We'll do more complicated counting later,
+//but for this problem the value is simply 0.
+
+public Map<String, Integer> word0(String[] strings) {
+  Map<String, Integer> map = new HashMap<String, Integer>();
+  for(String s: strings){
+    map.put(s,0);
+  }
+return map;
+}
+
+//---------------------------------------------------------------
+
+//--Map-2 > wordLen--
+//Given an array of strings, return a Map<String, Integer> containing a key for every different string in the array
+//and the value is that string's length.
+
+public Map<String, Integer> wordLen(String[] strings) {
+  Map<String,Integer> map = new HashMap<String, Integer>();
+  for(String s: strings){
+    map.put(s,s.length());
+  }
+  return map;
+}
+
+//-------------------------------------------------------------
